9aa8cd680658e7ada8b8467e9c2fe2be7917af60
CarlosAHzBt <carlos.hb@culiacan.tecnm.mx>
2024-05-08 17:52:09 -0700

    Refactor Main.py para primero procesar las imagenes con el modelo y despues el procesamiento de los baches detectados. Bache.py diametro min. aumentado a 150mm

--- Main.py
+++ Main.py
@@ -9,7 +9,6 @@ import csv
 import logging
 import sys
 
-
 # Configuración básica de logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -18,13 +17,14 @@ class Main:
         self.path_bag_folder = path_bag_folder
         self.output_folder = output_folder
         self.modelo = None
-        self.ruta_modelo ="model_state_dictV13.pth"  # Ruta del modelo entrenado
+        self.ruta_modelo = "model_state_dictV13.pth"  # Ruta del modelo entrenado
         self.lista_baches = []
 
     def run(self):
         self.extraccion_informacion()
         self.cargar_modelo()
         self.aplicar_modelo()
+        self.procesar_baches()
         self.aplicar_recorte_a_imagenes_que_contengan_bache()
         self.generar_documento_de_deterioros()
         self.borrar_todos_los_archivos_extraidos_al_terminar()
@@ -46,6 +46,7 @@ class Main:
         self.modelo = modelo_loader.cargar_modelo(ruta_modelo)
         self.modelo.to('cuda' if torch.cuda.is_available() else 'cpu')
         logging.info("Modelo cargado y transferido a " + ('CUDA' if torch.cuda.is_available() else 'CPU'))
+
     def aplicar_modelo(self):
         segmentador = ModeloSegmentacion(self.modelo)
         administrador_archivos = AdministradorArchivos("Extraccion")
@@ -57,11 +58,17 @@ class Main:
                 for i, coord in enumerate(coordenadas_baches):
                     id_bache = f"{os.path.splitext(os.path.basename(ruta_imagen))[0]}_{i}"
                     bag_de_origen = administrador_archivos.obtener_bag_de_origen(ruta_imagen, self.path_bag_folder)
-                    bache = Bache(ruta_carpeta_bag, bag_de_origen, self.output_folder ,ruta_imagen, id_bache, coord)
-                    if bache.procesar_bache():
-                        logging.info(f"El diametro máximo del bache {bache.id_bache} es {bache.diametro_bache} mm procedente del bag {bache.bag_de_origen}.")
-                        if bache.profundidad_del_bache_estimada < -0.009:
-                            self.lista_baches.append(bache)
+                    bache = Bache(ruta_carpeta_bag, bag_de_origen, self.output_folder, ruta_imagen, id_bache, coord)
+                    self.lista_baches.append(bache)
+                    logging.info(f"Bache {id_bache} identificado en la imagen {ruta_imagen}.")
+
+    def procesar_baches(self):
+        logging.info("Procesando baches identificados.")
+        for bache in self.lista_baches:
+            if bache.procesar_bache():
+                logging.info(f"El diámetro máximo del bache {bache.id_bache} es {bache.diametro_bache} mm procedente del bag {bache.bag_de_origen}.")
+                if bache.profundidad_del_bache_estimada < -0.01:
+                    logging.info(f"Bache {bache.id_bache} con profundidad {bache.profundidad_del_bache_estimada} m agregado a la lista final.")
 
     def aplicar_recorte_a_imagenes_que_contengan_bache(self):
         logging.info("Aplicando recorte a imágenes que contengan baches y procesando nubes de puntos.")
